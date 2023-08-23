529859e62ae1aa00c5b6b03d795d7626d07e5874
Matjaz Prtenjak <matjaz.prtenjak@endava.com>
2023-08-23 18:54:45 +0200

    STEP 1: Unzipping the initial project

---
+++ Simple CRUD/Main.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Simple_CRUD
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_add = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_firstname = new System.Windows.Forms.TextBox();
+            this.txt_lastname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_address = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(73, 577);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(129, 55);
+            this.btn_add.TabIndex = 0;
+            this.btn_add.Text = "ADD";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.Add);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(38, 181);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(699, 369);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.GetIdToDelete);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Edit);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 27);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Firstname";
+            // 
+            // txt_firstname
+            // 
+            this.txt_firstname.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_firstname.Location = new System.Drawing.Point(159, 63);
+            this.txt_firstname.Name = "txt_firstname";
+            this.txt_firstname.Size = new System.Drawing.Size(224, 35);
+            this.txt_firstname.TabIndex = 9;
+            // 
+            // txt_lastname
+            // 
+            this.txt_lastname.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_lastname.Location = new System.Drawing.Point(530, 66);
+            this.txt_lastname.Name = "txt_lastname";
+            this.txt_lastname.Size = new System.Drawing.Size(224, 35);
+            this.txt_lastname.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(405, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 27);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Lastname";
+            // 
+            // txt_address
+            // 
+            this.txt_address.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_address.Location = new System.Drawing.Point(372, 117);
+            this.txt_address.Name = "txt_address";
+            this.txt_address.Size = new System.Drawing.Size(224, 35);
+            this.txt_address.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(247, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 27);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Address";
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(252, 577);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(129, 55);
+            this.btn_update.TabIndex = 14;
+            this.btn_update.Text = "UPDATE";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.Update);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(410, 577);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(129, 55);
+            this.btn_delete.TabIndex = 15;
+            this.btn_delete.Text = "DELETE";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.Delete);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(590, 577);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(129, 55);
+            this.btn_clear.TabIndex = 16;
+            this.btn_clear.Text = "CLEAR";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.Clear);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 667);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.txt_address);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_lastname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_firstname);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_add);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_firstname;
+        private System.Windows.Forms.TextBox txt_lastname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_address;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_clear;
+    }
+}
+
---
+++ Simple CRUD/Main.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace Simple_CRUD
+{
+    public partial class Main : Form
+    {
+        SQLiteConnection conn;
+        SQLiteCommand cmd;
+        SQLiteDataAdapter adapter;
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        int id;
+        bool isDoubleClick = false;
+        String connectString;
+
+        public Main()
+        {
+            InitializeComponent();
+            connectString = @"Data Source=" + Application.StartupPath + @"\Database\crud.db;version=3";
+            GenerateDatabase();
+        }
+
+        private void Add(object sender, EventArgs e) {
+
+            if (txt_firstname.Text != "" || txt_lastname.Text != "" || txt_address.Text != "")
+            {
+                try
+                {
+                    conn = new SQLiteConnection(connectString);
+                    cmd = new SQLiteCommand();
+                    cmd.CommandText = @"INSERT INTO member (firstname, lastname, address) VALUES(@firstname, @lastname, @address)";
+                    cmd.Connection = conn;
+                    cmd.Parameters.Add(new SQLiteParameter("@firstname", txt_firstname.Text));
+                    cmd.Parameters.Add(new SQLiteParameter("@lastname", txt_lastname.Text));
+                    cmd.Parameters.Add(new SQLiteParameter("@address", txt_address.Text));
+                    conn.Open();
+
+                    int i = cmd.ExecuteNonQuery();
+
+                    if (i == 1)
+                    {
+                        MessageBox.Show("Successfully Created!");
+                        txt_firstname.Text = "";
+                        txt_lastname.Text = "";
+                        txt_address.Text = "";
+                        ReadData();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else {
+                MessageBox.Show("Required Field!");
+            }
+
+        }
+
+        private void GenerateDatabase() {
+            String path = Application.StartupPath + @"\Database\crud.db";
+            if (!File.Exists(path))
+            {
+                conn = new SQLiteConnection(connectString);
+                conn.Open();
+                string sql = "CREATE TABLE member (ID INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT, address TEXT)";
+                cmd = new SQLiteCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+
+        private void ReadData() {
+            try
+            {
+                conn = new SQLiteConnection(connectString);
+                conn.Open();
+                cmd = new SQLiteCommand();
+                String sql = "SELECT * FROM member";
+                adapter = new SQLiteDataAdapter(sql, conn);
+                ds.Reset();
+                adapter.Fill(ds);
+                dt = ds.Tables[0];
+                dataGridView1.DataSource = dt;
+                conn.Close();
+                dataGridView1.Columns[1].HeaderText = "Firstname";
+                dataGridView1.Columns[2].HeaderText = "Lastname";
+                dataGridView1.Columns[3].HeaderText = "Address";
+                dataGridView1.Columns[0].Visible = false;
+                dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                dataGridView1.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                dataGridView1.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            ReadData();
+        }
+
+        private void Edit(object sender, DataGridViewCellEventArgs e) {
+            id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+            txt_firstname.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            txt_lastname.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            txt_address.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            isDoubleClick = true;
+        }
+
+        private void GetIdToDelete(object sender, DataGridViewCellEventArgs e) {
+            id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+            isDoubleClick = false;
+            txt_firstname.Text = "";
+            txt_lastname.Text = "";
+            txt_address.Text = "";
+        }
+
+
+        private void Update(object sender, EventArgs e) {
+            if(isDoubleClick) { 
+                try {
+                    conn.Open();
+                    cmd = new SQLiteCommand();
+                    cmd.CommandText = @"UPDATE member set firstname=@firstname, lastname=@lastname, address=@address WHERE ID='"+ id +"'";
+                    cmd.Connection = conn;
+                    cmd.Parameters.AddWithValue("@firstname", txt_firstname.Text);
+                    cmd.Parameters.AddWithValue("@lastname", txt_lastname.Text);
+                    cmd.Parameters.AddWithValue("@address", txt_address.Text);
+                    int i = cmd.ExecuteNonQuery();
+
+                    if (i == 1)
+                    {
+                        MessageBox.Show("Successfully Updated!");
+                        txt_firstname.Text = "";
+                        txt_lastname.Text = "";
+                        txt_address.Text = "";
+                        ReadData();
+                        id = 0;
+                        dataGridView1.ClearSelection();
+                        dataGridView1.CurrentCell = null;
+                        isDoubleClick = false;
+                    }
+
+                    conn.Close();
+                }
+                catch (Exception ex) {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void Delete(object sender, EventArgs e) {
+            DialogResult dialogResult = MessageBox.Show("Do you to delete this record?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                try
+                {
+                    conn = new SQLiteConnection(connectString);
+                    conn.Open();
+                    cmd = new SQLiteCommand();
+                    cmd.CommandText = @"DELETE FROM member WHERE ID='" + id + "'";
+                    cmd.Connection = conn;
+                    int i = cmd.ExecuteNonQuery();
+                    if (i == 1)
+                    {
+                        MessageBox.Show("Successfully Deleted!");
+                        id = 0;
+                        dataGridView1.ClearSelection();
+                        dataGridView1.CurrentCell = null;
+                        ReadData();
+                        dataGridView1.ClearSelection();
+                        dataGridView1.CurrentCell = null;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                
+            }
+
+        }
+
+        private void Clear(object sender, EventArgs e)
+        {
+            id = 0;
+            txt_firstname.Text = "";
+            txt_lastname.Text = "";
+            txt_address.Text = "";
+            dataGridView1.ClearSelection();
+            dataGridView1.CurrentCell = null;
+            isDoubleClick = false;
+        }
+    }
+}
