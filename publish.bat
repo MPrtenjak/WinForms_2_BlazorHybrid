5ac54987eac14c7d34cf85c6d6c1634515422ee0
Matjaz Prtenjak <matjaz.prtenjak@endava.com>
2023-08-23 19:44:16 +0200

    STEP 4: EXE File

--- publish.bat
+++ publish.bat
@@ -1,8 +1,8 @@
 MsBuild.exe /t:Build /p:Configuration=Release
 cd ExecutableFiles
-rd /q /s STEP3
-md STEP3
-cd STEP3
+rd /q /s STEP4
+md STEP4
+cd STEP4
 xcopy "..\..\Simple CRUD\bin\Release" /S /E 
 rd /q /s Database 
 cd ..\..
