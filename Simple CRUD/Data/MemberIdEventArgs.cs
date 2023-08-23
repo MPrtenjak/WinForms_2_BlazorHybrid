9d3940bd0a51059e4d06e1050f96cb3090e9b3da
Matjaz Prtenjak <matjaz.prtenjak@endava.com>
2023-08-23 20:22:35 +0200

    STEP 12: Communicating from Blazor form back to legacy application

---
+++ Simple CRUD/Data/MemberIdEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Simple_CRUD.Data
+{
+    internal class MemberIdEventArgs : EventArgs
+    {
+        public long MemberId { get; init; }
+
+        public MemberIdEventArgs(long memberId)
+        {
+            MemberId = memberId;
+        }
+    }
+}
--- Simple CRUD/Data/MessageBroker.cs
+++ Simple CRUD/Data/MessageBroker.cs
@@ -5,10 +5,22 @@ namespace Simple_CRUD.Data
     internal class MessageBroker
     {
         public event EventHandler DatabaseChangedEvent;
+        public event EventHandler EditMemberEvent;
+        public event EventHandler DeleteMemberEvent;
 
         public void NotifyDatabaseChange()
         {
             DatabaseChangedEvent?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void EditMember(long memberId)
+        {
+            EditMemberEvent?.Invoke(this, new MemberIdEventArgs(memberId));
+        }
+
+        public void DeleteMember(long memberId)
+        {
+            DeleteMemberEvent?.Invoke(this, new MemberIdEventArgs(memberId));
         }
     }
 }
