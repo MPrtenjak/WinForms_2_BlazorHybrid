8e9cf8522f799989d2ed392676ed8cc07e1c568b
Matjaz Prtenjak <matjaz.prtenjak@endava.com>
2023-08-23 20:18:21 +0200

    STEP 9: Implement synchronization between Blazor Forms

--- Simple CRUD/Data/CounterData.cs
+++ Simple CRUD/Data/CounterData.cs
@@ -1,11 +1,18 @@
-﻿namespace Simple_CRUD.Data
+﻿using System;
+
+namespace Simple_CRUD.Data
 {
     internal class CounterData
     {
         public int CurrentValue { get; private set; }
+        public event EventHandler CurrentValueChangedEvent;
 
         public CounterData() => CurrentValue = 0;
 
-        public void Increment() => CurrentValue++;
+        public void Increment()
+        {
+            CurrentValue++;
+            CurrentValueChangedEvent?.Invoke(this, EventArgs.Empty);
+        }
     }
 }
